835fccecab22589cd819a7e3e59047e642911e02
Fabian Ewald <fabian.ewald@infopark.de>
2015-07-13 15:22:51 +0200

    removed slider, because it reloads the entire parent obj and this can take ages.

--- app/models/two_column_widget.rb
+++ app/models/two_column_widget.rb
@@ -2,9 +2,5 @@ class TwoColumnWidget < Widget
   attribute :column_1, :widgetlist
   attribute :column_2, :widgetlist
   attribute :column_1_width, :string, default: '6'
-
-  def column_2_width
-    12 - self.column_1_width.to_i
-  end
-
+  attribute :column_2_width, :string, default: '6'
 end
