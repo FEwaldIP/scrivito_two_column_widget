c9a5e750cf84a99134db6fc1416302a39b66b349
Fabian Ewald <fabian.ewald@infopark.de>
2015-07-13 15:44:45 +0200

    add some more attributes for the views

--- app/models/two_column_widget.rb
+++ app/models/two_column_widget.rb
@@ -3,4 +3,13 @@ class TwoColumnWidget < Widget
   attribute :column_2, :widgetlist
   attribute :column_1_width, :string, default: '6'
   attribute :column_2_width, :string, default: '6'
+
+  attribute :column_1_desktop_width, :string
+  attribute :column_2_desktop_width, :string
+
+  attribute :column_1_tablet_width, :string
+  attribute :column_2_tablet_width, :string
+
+  attribute :column_1_mobile_width, :string
+  attribute :column_2_mobile_width, :string  
 end
